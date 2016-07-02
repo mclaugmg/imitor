2d6ef9f6d0c03bdf4abffc321a76ab72598a8786
Matthew McLaughlin <Matthew McLaughlin>
2016-07-02 15:51:04 -0700

    changed to all vanilla JS and deleted jQuery

--- client/browser.js
+++ client/browser.js
@@ -8,43 +8,44 @@ let alphaAvg = 0;
 let betaAvg = 0;
 let gammaAvg = 0;
 
-let alphaDataArray = [0, 1, 2];
-let betaDataArray = [0, 1, 2];
-let gammaDataArray = [0, 1, 2];
+let alphaDataArray = [0, 0, 0];
+let betaDataArray = [0, 0, 0];
+let gammaDataArray = [0, 0, 0];
 
 // Running average function stacks most recent acceleration points and calcs avg
 function calculateRunningAverage(gyroscopeDataObject) {
   alphaDataArray.shift();
   alphaDataArray.push(gyroscopeDataObject.alpha);
-  alphaAvg = alphaDataArray.reduce((a, b) => {return a + b; }) / 3;
+  alphaAvg = Math.round(alphaDataArray.reduce((a, b) => {return a + b; }) / 3);
   betaDataArray.shift();
   betaDataArray.push(gyroscopeDataObject.beta);
-  betaAvg = betaDataArray.reduce((a, b) => {return a + b; }) / 3;
+  betaAvg = Math.round(betaDataArray.reduce((a, b) => {return a + b; }) / 3);
   gammaDataArray.shift();
   gammaDataArray.push(gyroscopeDataObject.gamma);
-  gammaAvg = gammaDataArray.reduce((a, b) => {return a + b; }) / 3;
+  gammaAvg = Math.round(gammaDataArray.reduce((a, b) => {return a + b; }) / 3);
 }
 
 // Instantiate acceleration handler
 imperio.desktopGyroHandler(imperio.socket, calculateRunningAverage);
 
 let counter = 0;
-let alphaElement = document.getElementById('alpha-angle');
-let betaElement = document.getElementById('beta-angle');
-let gammaElement = document.getElementById('gamma-angle');
-let bodyElement = document.querySelector('body');
+const alphaElement = document.getElementById('alpha-angle');
+const betaElement = document.getElementById('beta-angle');
+const gammaElement = document.getElementById('gamma-angle');
+const bodyElement = document.querySelector('body');
+const cube = document.getElementById('cube');
 
 // Removes and adds one data point to each dataset in the chart
 function addData() {
   counter++;
   if (counter % 20 === 0) {
-    bodyElement.css('background-color', 'yellow');
+    bodyElement.style['background-color'] = 'yellow';
   }
   if (counter % 40 === 0) {
-    bodyElement.css('background-color', 'white');
+    bodyElement.style['background-color'] = 'yellow';
     counter = 0;
   }
-  $('#cube').css('transform', `translateZ(-100px) rotateX(${gammaAvg}deg) rotateY(${alphaAvg}deg) rotateZ(${betaAvg}deg)`);
+  cube.style.transform = `translateZ(-100px) rotateX(${gammaAvg}deg) rotateY(${alphaAvg}deg) rotateZ(${betaAvg}deg)`;
   // $('#cube').css('transform', `translateZ(-100px) rotateZ(${counter}deg)`);
   alphaElement.innerHTML = `${alphaAvg}`;
   betaElement.innerHTML = `${betaAvg}`;
